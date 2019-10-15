c9c4fa1f8ef6a14b8e3b970aaef4fe162a3860e7
Alex Kotov <kotovalexarian@gmail.com>
2019-10-15 16:59:58 +0500

    Add Postgres credentials

--- action_plugins/validate_vars.py
+++ action_plugins/validate_vars.py
@@ -11,12 +11,25 @@ class ActionModule(ActionBase):
         'deploy_user',
         'env',
         'group',
+        'pg_db',
+        'pg_host',
+        'pg_pass',
+        'pg_port',
+        'pg_user',
+        'restrict',
         'root_dir',
         'service',
         'user',
     ]
 
     common_name_re = re.compile(r'^[a-z_][a-z0-9_-]{0,30}(\$|[a-z0-9_-])?$')
+
+    domain_name_re = re.compile(
+        r'^(?:[a-z0-9]'  # First character of the domain
+        r'(?:[a-z0-9-_]{0,61}[a-z0-9])?\.)'  # Sub domain + hostname
+        r'+[a-z0-9][a-z0-9-_]{0,61}'  # First 61 characters of the gTLD
+        r'[a-z0-9]$'  # Last character of the gTLD
+    )
 
     def run(self, tmp=None, task_vars=None):
         if task_vars is None:
@@ -57,6 +70,38 @@ class ActionModule(ActionBase):
             return 'is not str'
         if not self.common_name_re.fullmatch(value):
             return 'has invalid format'
+
+    def validate_pg_db(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.common_name_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_pg_host(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.domain_name_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_pg_pass(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+
+    def validate_pg_port(self, value):
+        if not isinstance(value, int):
+            return 'is not int'
+        if not (1 <= value < 2**16):
+            return 'is invalid'
+
+    def validate_pg_user(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.common_name_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_restrict(self, value):
+        if not isinstance(value, bool):
+            return 'is not bool'
 
     def validate_root_dir(self, value):
         if not isinstance(value, str):
